71a7b883aaff8a5d7901895f436501c70d72f793
farukobradovic <faruk_95@msn.com>
2019-10-11 20:29:00 +0200

    Updates

---
+++ precache-manifest.35129817be6679c1af4fcb3f4503598e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7637affa64bfa2fe2183528ab185bf2f",
+    "url": "/fanaticoapp/index.html"
+  },
+  {
+    "revision": "d15f47ef2a6ff20ac248",
+    "url": "/fanaticoapp/static/css/main.c9eeccbf.chunk.css"
+  },
+  {
+    "revision": "ecaa217a515894d54c6d",
+    "url": "/fanaticoapp/static/js/2.226757a1.chunk.js"
+  },
+  {
+    "revision": "d15f47ef2a6ff20ac248",
+    "url": "/fanaticoapp/static/js/main.9ac67fa0.chunk.js"
+  },
+  {
+    "revision": "21172fbe90620697c530",
+    "url": "/fanaticoapp/static/js/runtime-main.76fb392b.js"
+  }
+]);
